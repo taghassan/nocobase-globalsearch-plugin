bc4605abfb5a54d163d92f7e16e4380ac8ccf7e0
TajEldeen Hassan Ali <taghassan54@gmail.com>
2026-08-29 03:13:51 +0400

    fixes

--- dist/server/resourcers/global-search.d.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Next } from '@nocobase/actions';
-declare const _default: {
-    name: string;
-    actions: {
-        search(ctx: Context, next: Next): Promise<any>;
-    };
-};
-export default _default;
